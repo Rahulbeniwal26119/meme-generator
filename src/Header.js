3b8d15755353431a1eb0717d44d506f273083c03
Rahul Beniwal <rahulbeniwal18112002@gmail.com>
2021-01-10 13:34:48 +0530

    adding header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component} from "react";
+import './Header.css';
+export default  function Header(props)
+{
+    return (
+        <header>
+            <h3 className="meme-generator">
+                Meme Generator
+            </h3>
+        </header>
+    )
+}
